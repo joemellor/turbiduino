a71f7e2f4459a824484f6bf6d781b7a861f2cd83
Gaurav Gupta <g.gupta@mail.utoronto.ca>
2013-05-10 17:32:38 -0700

    Fixed incorrect naming of php files, added graphs
    The display.php and interface.php were mixed around in the repo.
    Resolved this issue and updated display.php to display a graph of the
    run using pChart.

--- interface/display.php
+++ interface/display.php
@@ -1,28 +1,70 @@
 <?php
 
-//connect to the SQL database and store a list of runIDs in array $rows.
+include("pChart2.1.3/class/pDraw.class.php");
+include("pChart2.1.3/class/pImage.class.php");
+include("pchart2.1.3/class/pData.class.php");
+
+$mydata = new pData();
+$time = array(); $doublings = array();
+
+//get the unique id of the run requested and access run data from database.
+$runid = $_POST["runid"];
 $con = pg_connect("host=localhost dbname=turbiduino user=joemellor password=lamarama");
-$query = "SELECT DISTINCT run_id FROM READINGS ORDER BY run_id DESC";
+$query = "SELECT * FROM readings WHERE run_id IN ('$runid')";
 $result = pg_query($con, $query);
+//store the resulting data in the array $rows.
 $rows = pg_fetch_all($result);
 
-echo  generateSelect('runid', $rows, $con);
+//lay out the 'recent readings' table.
+echo "number of readings: ";
+$size = count($rows);
+echo $size;
+echo '</br></br> recently: (most recent at top) </br> <table border = "1">';
+echo "<tr><td> baseline reading </td> <td> current reading </td> <td> hours elapsed </td> <td> temperature(C) </td> <td> doublings </td> </tr>";
 
-
-//generates a drop-down menu by formatting runIDs and passes on a connection to the database to the next php script.
-function generateSelect($name = '', $options = array(),$con) {
-    $html = '<form method = "post" action = "display.php">';
-    $html .= '<select name="'.$name.'">';
-
-    foreach ($options as $key => $value) {
-        $runid = $value[run_id];
-        $str = substr($runid,6,2)."/". substr($runid,4,2)."/".substr($runid,0,4)." on ".substr($runid,8,2).":".substr($runid,10);
-        $html .= '<option value='.$runid.'>'.$str.'</option>';
-        }
-
-    $html .= '</select>';
-    $html .= '<INPUT TYPE = "submit" name = "submit" /> </form>';
-    return $html;
+//fill in the 'recent readings' table.
+for($counter = $size-1; $counter > $size - 6; $counter -= 1) {
+        $value = $rows[$counter];
+        //convert seconds to hours and format to 3dp.
+        $hours = $value[time]/3600;
+        $hours = number_format((float)$hours, 3, '.', ',');
+        echo "<tr> <td>".$value[baseline_read]."</td> <td>".$value[current_read]."</td> <td>".$hours."</td> <td>".$value[temperature]."</td> <td>".$value[doublings]."</td> </tr>";
 }
+echo "</table>";
+
+
+//initialize the 'all readings' table.
+echo "</br></br> all readings: (most recent at bottom)";
+echo '<div style="float:left;"> </br><table border = "1">';
+echo "<tr><td> baseline reading </td> <td> current reading </td> <td> hours elapsed </td> <td> temperature(C) </td> <td> doublings </td> </tr>";
+
+//fill in the 'all readings' table.
+foreach($rows as $value) {
+        $hours = $value[time]/3600;
+        $hours = number_format((float)$hours, 3, '.', ',');
+
+echo "<tr> <td>".$value[baseline_read]."</td> <td>".$value[current_read]."</td> <td>".$hours."</td> <td>".$value[temperature]."</td> <td>".$value[doublings]."</td> </tr>";
+
+        $time[] = $hours;
+        $doublings[] = $value[doublings];
+         }
+echo "</table> </div>";
+
+$mydata->addPoints($time, "Hours elapsed");
+$mydata->addPoints($doublings, "doublings");
+$mydata->setAbscissa("Hours elapsed");
+$mydata->setAxisName(0, "Doublings");
+$mydata->setXAxisName("Time");
+$mypicture = new pImage(700, 230, $mydata);
+$mypicture->setFontProperties(array("FontName"=>"pChart2.1.3/fonts/Forgotte.ttf","FontSize"=>11));
+$mypicture->setGraphArea(0,0,670,210);
+
+$max_x_labels = 13;
+$skip = floor(count($time)/13);
+$mypicture->drawScale(array("LabelSkip"=>$skip));
+
+$mypicture->drawSplineChart();
+$mypicture->Render("graph.png");
+echo '<div style="float:right;"> <img src = "graph.png"> </div>';
 
 ?>
--- interface/interface.php
+++ interface/interface.php
@@ -1,42 +1,28 @@
 <?php
 
-//get the unique id of the run requested and access run data from database.
-$runid = $_POST["runid"];
+//connect to the SQL database and store a list of runIDs in array $rows.
 $con = pg_connect("host=localhost dbname=turbiduino user=joemellor password=lamarama");
-$query = "SELECT * FROM readings WHERE run_id IN ('$runid')";
+$query = "SELECT DISTINCT run_id FROM READINGS ORDER BY run_id DESC";
 $result = pg_query($con, $query);
-//store the resulting data in the array $rows.
 $rows = pg_fetch_all($result);
 
-//lay out the 'recent readings' table.
-echo "number of readings: ";
-$size = count($rows);
-echo $size;
-echo '</br></br> recently: (most recent at top) </br> <table border = "1">';
-echo "<tr><td> baseline reading </td> <td> current reading </td> <td> hours elapsed </td> <td> temperature(C) </td> <td> doublings </td> </tr>";
+echo  generateSelect('runid', $rows, $con);
 
-//fill in the 'recent readings' table.
-for($counter = $size-1; $counter > $size - 6; $counter -= 1) {
-        $value = $rows[$counter];
-        //convert seconds to hours and format to 3dp.
-        $hours = $value[time]/3600;
-        $hours = number_format((float)$hours, 3, '.', ',');
-        echo "<tr> <td>".$value[baseline_read]."</td> <td>".$value[current_read]."</td> <td>".$hours."</td> <td>".$value[temperature]."</td> <td>".$value[doublings]."</td> </tr>";
+
+//generates a drop-down menu by formatting runIDs and passes on a connection to the database to the next php script.
+function generateSelect($name = '', $options = array(),$con) {
+    $html = '<form method = "post" action = "display.php">';
+    $html .= '<select name="'.$name.'">';
+
+    foreach ($options as $key => $value) {
+        $runid = $value[run_id];
+        $str = substr($runid,6,2)."/". substr($runid,4,2)."/".substr($runid,0,4)." on ".substr($runid,8,2).":".substr($runid,10);
+        $html .= '<option value='.$runid.'>'.$str.'</option>';
+        }
+
+    $html .= '</select>';
+    $html .= '<INPUT TYPE = "submit" name = "submit" /> </form>';
+    return $html;
 }
-echo "</table>";
-
-
-//initialize the 'all readings' table.
-echo "</br></br> all readings: (most recent at bottom)";
-echo '</br><table border = "1">';
-echo "<tr><td> baseline reading </td> <td> current reading </td> <td> hours elapsed </td> <td> temperature(C) </td> <td> doublings </td> </tr>";
-
-//fill in the 'all readings' table.
-foreach($rows as $value) {
-        $hours = $value[time]/3600;
-        $hours = number_format((float)$hours, 3, '.', ',');
-
-echo "<tr> <td>".$value[baseline_read]."</td> <td>".$value[current_read]."</td> <td>".$hours."</td> <td>".$value[temperature]."</td> <td>".$value[doublings]."</td> </tr>"; }
-echo "</table>";
 
 ?>
