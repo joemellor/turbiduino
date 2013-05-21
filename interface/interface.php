b70814793f141539230856cd9b7b32b82633d479
Gaurav Gupta <g.gupta@mail.utoronto.ca>
2013-05-21 14:25:04 -0700

    added php script, updated php documentation
    Added the date.php script to the repository and added more documentation
    to the interface.php script.

--- interface/interface.php
+++ interface/interface.php
@@ -6,16 +6,20 @@ $query = "SELECT DISTINCT run_id FROM READINGS ORDER BY run_id DESC";
 $result = pg_query($con, $query);
 $rows = pg_fetch_all($result);
 
+//pass on the connection handle and rows array to create a menu.
 echo  generateSelect('runid', $rows, $con);
 
 
 //generates a drop-down menu by formatting runIDs and passes on a connection to the database to the next php script.
 function generateSelect($name = '', $options = array(),$con) {
+    //call the script 'display.php' on submit.
     $html = '<form method = "post" action = "display.php">';
     $html .= '<select name="'.$name.'">';
 
+    //iterate through $rows, allowing you to get data for each reading.
     foreach ($options as $key => $value) {
         $runid = $value[run_id];
+	   //parse the string runID into a readable form.
         $str = substr($runid,6,2)."/". substr($runid,4,2)."/".substr($runid,0,4)." on ".substr($runid,8,2).":".substr($runid,10);
         $html .= '<option value='.$runid.'>'.$str.'</option>';
         }
